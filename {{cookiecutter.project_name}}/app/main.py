7249a3f3d99b9a98cdd30524f104b20dd6b91788
7ym0n.q6e <bb.qnyd@gmail.com>
2020-12-11 11:09:35 +0800

    fixed: main settings

--- {{cookiecutter.project_name}}/app/main.py
+++ {{cookiecutter.project_name}}/app/main.py
@@ -4,24 +4,25 @@
 # Author: {{cookiecutter.author}} <{{cookiecutter.email}}>
 #
 
-from app.core.config import (ALLOWED_HOSTS, API_PREFIX, DEBUG, PROJECT_NAME,
-                             VERSION)
-from app.core.events import create_start_app_handler, create_stop_app_handler
-from app.core.http_error import http_error_handler
-from app.core.routers import router as api_router
-from app.core.validation_error import http422_error_handler
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException
 from starlette.middleware.cors import CORSMiddleware
 
+from app.core.config import settings
+from app.core.events import create_start_app_handler, create_stop_app_handler
+from app.core.http_error import http_error_handler
+from app.core.routers import router as api_router
+from app.core.validation_error import http422_error_handler
+
 
 def get_application() -> FastAPI:
-    application = FastAPI(title=PROJECT_NAME, debug=DEBUG, version=VERSION)
+    application = FastAPI(title=settings.PROJECT_NAME,
+                          debug=settings.DEBUG, version=settings.VERSION)
 
     application.add_middleware(
         CORSMiddleware,
-        allow_origins=ALLOWED_HOSTS or ["*"],
+        allow_origins=settings.ALLOWED_HOSTS or ["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
@@ -36,7 +37,7 @@ def get_application() -> FastAPI:
     application.add_exception_handler(
         RequestValidationError, http422_error_handler)
 
-    application.include_router(api_router, prefix=API_PREFIX)
+    application.include_router(api_router, prefix=settings.API_PREFIX)
 
     return application
 
