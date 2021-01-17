d92487d0c07b3bb4a16fc103cf70d88119906915
7ym0n.q6e <bb.qnyd@gmail.com>
2021-01-17 11:41:03 +0800

    fix: pytest error and .env var

--- {{cookiecutter.project_name}}/app/tests/test_main.py
+++ {{cookiecutter.project_name}}/app/tests/test_main.py
@@ -13,6 +13,6 @@ from httpx import AsyncClient
 @pytest.mark.asyncio
 async def test_root():
     async with AsyncClient(app=app, base_url="http://localhost:8000") as ac:
-        response = await ac.get("/api")
+        response = await ac.get("/docs")
 
     assert response.status_code == 200
